53af3203c8d58a99d3bb7237a98f2f238f0a37b2
mxchainsaw <quit>
2023-03-27 02:07:52 +0200

    Add another hasher bcrypt

---
+++ WebUI/WebUI/Services/BcryptPasswordHasher.cs
@@ -0,0 +1,16 @@
+﻿using WebUI.Services.Contracts;
+
+namespace WebUI.Services
+{
+    public class BcryptPasswordHasher : IPasswordHasher
+    {
+        public string HashPassword(string password)
+        {           
+            // Hash the password using bcrypt with a cost factor of 10
+            var hashedPassword = BCrypt.Net.BCrypt.HashPassword(password, workFactor: 10);
+
+            // Return the hashed password as a string
+            return hashedPassword;
+        }
+    }
+}
