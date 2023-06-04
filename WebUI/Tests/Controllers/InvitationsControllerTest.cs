91661481f92146ae312e3a7f05bd4a37d20e39e5
mxchainsaw <quit>
2023-06-05 01:23:32 +0200

    Add unit tests to WebApp, InvitationController

---
+++ WebUI/Tests/Controllers/InvitationsControllerTest.cs
@@ -0,0 +1,69 @@
+﻿using AutoMapper;
+using FluentValidation;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using WebUI.Clients.Contracts;
+using WebUI.Controllers;
+using WebUI.Models.Dtos;
+using WebUI.Models.Enums;
+using WebUI.Models.ViewModels;
+using WebUI.Profiles;
+using WebUI.Validators;
+
+namespace Tests.Controllers
+{
+    public class InvitationsControllerTest
+    {
+        private readonly IMapper _mapper;
+        private readonly IValidator<CreateInvitationViewModel> _validator;
+        private readonly Mock<IHttpContextAccessor> _mockHttpContextAccessor;
+        private readonly Mock<IInvitationsClient> _mockInvitationsClient;
+        private readonly InvitationsController _sut;
+
+        public InvitationsControllerTest()
+        {
+            var profile = new AutomapperProfile();
+            var config = new MapperConfiguration(cfg => cfg.AddProfile(profile));
+            _mapper = new Mapper(config);
+            _mockInvitationsClient = new();
+            _mockHttpContextAccessor = new();
+            _validator = new InvitationValidator();
+            _sut = new(_mockInvitationsClient.Object, _mapper, _mockHttpContextAccessor.Object, _validator);
+        }
+
+        [Fact]
+        public async Task CreatePost_WhenInvalidModelState_ShouldReturnsBadRequest()
+        {
+            // Arrange
+            _sut.ModelState.AddModelError("Test", "Test");
+            var viewModel = new CreateInvitationViewModel();
+
+            // Act
+            var result = await _sut.Create(viewModel, Guid.NewGuid());
+
+            // Assert
+            Assert.IsType<BadRequestObjectResult>(result);
+        }
+
+        [Fact]
+        public async Task CreatePost_ValidRequest_ReturnsRedirectToActionResult()
+        {
+            // Arrange
+            var viewModel = new CreateInvitationViewModel()
+            {
+                Email = "Test@gmail.com",
+                Role = RoleToInvite.User
+            };            
+            _mockInvitationsClient.Setup(x => x.CreateInvitation(It.IsAny<InvitationDto>())).ReturnsAsync(new InvitationDto());
+
+            // Act
+            var result = await _sut.Create(viewModel, Guid.NewGuid());
+
+            // Assert
+            var redirectResult = Assert.IsType<RedirectToActionResult>(result);            
+            Assert.Equal("Home", redirectResult?.ControllerName);
+            Assert.Equal("Index", redirectResult?.ActionName);            
+        }
+    }
+}
